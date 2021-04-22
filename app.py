5369612466990f09fec184ac1160dfa199046693
Anh Minh <annminn104@gmail.com>
2021-04-22 11:44:51 +0700

    first commit

--- app.py
+++ app.py
@@ -302,4 +302,4 @@ def video_feed():
 
 if __name__ == "__main__":
     #ssl_context=('cert.pem', 'key.pem')
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="8080", ssl_context='adhoc')
